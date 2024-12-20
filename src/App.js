44d4b84afcd799037ad10b5d5ea00e67d1bda02b
somrathumayan <somrathumayan@gmail.com>
2024-12-21 00:48:01 +0600

    styled 3 images to see better

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import Data from "./../src/assets/data/jsondata.json";
 
 function App() {
   return (
-    <div className="">
-      
+    <div style={{display:"flex", alignItems:"center", justifyContent:"    center"}}>
       {Data.map(da => (
-        // <h1 key={da.id}>{da.img}</h1>
-        <div key={da.id}>
+        <div key={da.id} style={{display:"flex",float:"left", height:"200px", border:"5px solid tomato", margin:"5px", textAlign:"center"}}>
           <img src={da.img} alt={da.alt} width={200}/>
         </div>
       ))}
